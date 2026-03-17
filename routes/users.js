db298a18170fd1f2e4b9f2ade6e5b888a300cd0f
vithursan6 <vithursanm@gmail.com>
2026-03-16 20:09:14 -0400

    added get routes

--- routes/users.js
+++ routes/users.js
@@ -26,13 +26,14 @@ let users = [
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
   // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  res.send(users);//This line is to be replaced with actual return value
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  let filtered_users = users.filter((user) => user.email === email);
+  res.send(filtered_users);//This line is to be replaced with actual return value
 });
 
 
